2f92b56b7c6babbe564e62a89f164dce2076937b
Farhad Faramarzi <farhad.public@gmail.com>
2017-08-08 11:04:45 +0430

    Initial Commit

---
+++ Notifire/Notifire.h
@@ -0,0 +1,19 @@
+//
+//  Notifire.h
+//  Notifire
+//
+//  Created by Farhad on 8/8/17.
+//  Copyright © 2017 Farhad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notifire.
+FOUNDATION_EXPORT double NotifireVersionNumber;
+
+//! Project version string for Notifire.
+FOUNDATION_EXPORT const unsigned char NotifireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notifire/PublicHeader.h>
+
+
