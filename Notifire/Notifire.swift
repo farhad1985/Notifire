3a7add7e2ae35c1665552f420bd15b29d3efed02
Farhad Faramarzi <farhad.public@gmail.com>
2017-08-30 12:12:54 +0430

    add multiple line

--- Notifire/Notifire.swift
+++ Notifire/Notifire.swift
@@ -12,57 +12,67 @@ public class Notifire {
     
     public static let shared = Notifire()
     public let title = UILabel()
-    private var notifireView: UIView?
+    private var notifireView: UIView = UIView()
+    
     private var timer = 3
-    private let height = 100
+    private var height = 100
     private let overLap = 30
     private var navBar: UINavigationController?
     private var isNavBar = false
     private var hNav = 0
-
+    private var isShow = false
+    
+    private init() {}
+    
     public func show(type: NotifireType, message: String, timer: Int = 3) {
-        guard notifireView == nil else { return }
-        self.timer = timer
-        setupView(type: type)
-        title.text = message
-        animated()
+        if !isShow {
+            isShow = true
+            self.timer = timer
+            title.numberOfLines = 3
+            title.text = message
+            setupView(type: type)
+            animated()
+        }
         
     }
     
-    private func topViewController() -> UIViewController? {
-        var top = UIApplication.shared.keyWindow?.rootViewController
-            if let presented = top?.presentedViewController {
-                top = presented
-            } else if let nav = top as? UINavigationController {
-                navBar = nav
-                top = nav.visibleViewController
-                if let nav = navBar {
-                    hNav = Int(nav.navigationBar.frame.height)
-                }
-            } else if let tab = top as? UITabBarController {
-                top = tab.selectedViewController
+    private func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return navigationController
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return selected
             }
-        return top
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
     }
 
     private func setupView(type: NotifireType) {
         let frameTopVC = topViewController()?.view.frame
 
-        let frame = CGRect(x: 0, y: -height, width: Int((frameTopVC?.width)!), height: height)
-        notifireView = UIView(frame: frame)
-        notifireView?.backgroundColor = getColor(type: type)
-        notifireView?.layer.cornerRadius = 10.0
-        notifireView?.layer.shadowOpacity = 0.3
-        notifireView?.layer.shadowOffset = CGSize(width: 3, height: 3)
-        topViewController()?.view.addSubview(notifireView!)
-        notifireView?.addSubview(title)
+        let frame = CGRect(x: 0, y: -1000, width: Int((frameTopVC?.width)!), height: height)
+        notifireView.frame = frame
+        notifireView.backgroundColor = getColor(type: type)
+        notifireView.layer.cornerRadius = 10.0
+        notifireView.layer.shadowOpacity = 0.3
+        notifireView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        topViewController()?.view.addSubview(notifireView)
+        notifireView.addSubview(title)
         title.translatesAutoresizingMaskIntoConstraints = false
-        title.bottomAnchor.constraint(equalTo: (notifireView?.bottomAnchor)!, constant: -16).isActive = true
-        title.leftAnchor.constraint(equalTo: (notifireView?.leftAnchor)!, constant: 16).isActive = true
-        title.rightAnchor.constraint(equalTo: (notifireView?.rightAnchor)!, constant: -16).isActive = true
+        title.bottomAnchor.constraint(equalTo: (notifireView.bottomAnchor), constant: -16).isActive = true
+        title.leftAnchor.constraint(equalTo: (notifireView.leftAnchor), constant: 16).isActive = true
+        title.rightAnchor.constraint(equalTo: (notifireView.rightAnchor), constant: -16).isActive = true
         title.textColor = .white
+        notifireView.layoutSubviews()
+        height = Int(title.frame.height + 80)
+        notifireView.frame.size.height = CGFloat(height)
+        print(height)
     }
-  
+    
     private func getColor(type: NotifireType) -> UIColor {
         switch type {
         case .info: return UIColor(red: 9/255, green: 194/255, blue: 102/255, alpha: 1.0)
@@ -73,10 +83,10 @@ public class Notifire {
     
     private func animated() {
         UIView.animate(withDuration: 0.4, animations: {
-            self.notifireView?.frame.origin.y = CGFloat(-10 + self.hNav)
+            self.notifireView.frame.origin.y = CGFloat(-10 + self.hNav)
         }) { _ in
             UIView.animate(withDuration: 0.4, animations: {
-                self.notifireView?.frame.origin.y = (CGFloat)(-self.overLap + self.hNav)
+                self.notifireView.frame.origin.y = (CGFloat)(-self.overLap + self.hNav)
             })
             Timer.scheduledTimer(timeInterval: TimeInterval(self.timer), target: self, selector: #selector(self.dismiss), userInfo: nil, repeats: false)
         }
@@ -84,13 +94,13 @@ public class Notifire {
     
     @objc private func dismiss() {
         UIView.animate(withDuration: 0.4, animations: {
-            self.notifireView?.frame.origin.y = CGFloat(-10 + self.hNav)
+            self.notifireView.frame.origin.y = CGFloat(-10 + self.hNav)
         }) { _ in
             UIView.animate(withDuration: 0.4, animations: { 
-                self.notifireView?.frame.origin.y = -(CGFloat(self.height + self.overLap))
+                self.notifireView.frame.origin.y = -(CGFloat(self.height + self.overLap))
             }, completion: { _ in
-                self.notifireView?.removeFromSuperview()
-                self.notifireView = nil
+                self.notifireView.removeFromSuperview()
+                self.isShow = false
             })
         }
     }
